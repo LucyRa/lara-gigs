77769e04c6d297be50e285038df8cdaf975208a8
LucyRa <lucyramplin@gmail.com>
2022-07-01 13:13:34 +0100

    Starting to add filtering

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,11 +11,19 @@ class ListingController extends Controller
   | Get all of the listings, from the Listing model, 
   | and then render the listings view with the
   | retrieved data
+  | 
+  | Listing::latest()->get() = Get all listings and 
+  | order them by created_at, with most recent being 
+  | rendered first
+  | -- Adding the filter() method, with request(['tag', 'search']) as
+  |    a parameter, will return all listings that have
+  |    the defined tag or have a title/description/tag value which is
+  |    like the tag/search parameter
   */
   public function index() {
     return view('listings.index', [
         'heading' => 'Latest Listings',
-        'listings' => Listing::all()
+        'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
     ]);
   }
 
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Listing extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  /*
+  | Query the listings and refine by those which match a tag or search
+  | parameter given in the URL
+  |
+  | Acts on the laravel filter() method - see controller
+  */
+  public function scopeFilter($query, array $filters) {
+    // Filter by tag
+    if($filters['tag'] ?? false) {
+      $query->where('tags', 'like', '%' . request('tag') . '%');
+    }
+
+    // Filter by search
+    if($filters['search'] ?? false) {
+      $query->where('title', 'like', '%' . request('search') . '%')
+        ->orWhere('description', 'like', '%' . request('search') . '%')
+        ->orWhere('tags', 'like', '%' . request('search') . '%');
+    }
+  }
 }
