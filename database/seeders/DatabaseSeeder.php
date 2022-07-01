b7d71e159e7d4d03960625d13f9dbcabbc98c7c0
LucyRa <lucyramplin@gmail.com>
2022-07-01 11:10:08 +0100

    Database factory and seed - listings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,26 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston MA',
-            'email' => 'email@email.com',
-            'website' => 'https://lramplin.co.uk',
-            'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
-        ]);
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston MA',
+        //     'email' => 'email@email.com',
+        //     'website' => 'https://lramplin.co.uk',
+        //     'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
+        // ]);
 
-        Listing::create([
-            'title' => 'Full-Stack Engineer',
-            'tags' => 'laravel, backend, api',
-            'company' => 'Stark Industries',
-            'location' => 'New York, NY',
-            'email' => 'email2@email.com',
-            'website' => 'https://lramplin.co.uk',
-            'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
-        ]);
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'https://lramplin.co.uk',
+        //     'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
+        // ]);
+
+        Listing::factory(6)->create();
     }
 }
