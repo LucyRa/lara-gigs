3cf5bd194c7494a26e1d323079f41907bf916317
LucyRa <lucyramplin@gmail.com>
2022-07-01 11:00:40 +0100

    Creating a basic seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston MA',
+            'email' => 'email@email.com',
+            'website' => 'https://lramplin.co.uk',
+            'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
+        ]);
+
+        Listing::create([
+            'title' => 'Full-Stack Engineer',
+            'tags' => 'laravel, backend, api',
+            'company' => 'Stark Industries',
+            'location' => 'New York, NY',
+            'email' => 'email2@email.com',
+            'website' => 'https://lramplin.co.uk',
+            'description' => "Life finds a way. What do they got in there? King Kong? Just my luck, no ice. Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. Did he just throw my cat out of the window? Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should."
+        ]);
     }
 }
