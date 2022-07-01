2fa4132c6988153bf6247e21e8a8e93d58db67f2
LucyRa <lucyramplin@gmail.com>
2022-07-01 12:50:40 +0100

    Updating view structure and names to match controller

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
 /*
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
