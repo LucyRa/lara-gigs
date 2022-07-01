a24294673001f7e84dff953f071747307985723b
LucyRa <lucyramplin@gmail.com>
2022-07-01 10:18:24 +0100

    If directive, testing no data

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,11 +1,9 @@
 <h1>{{ $heading }}</h1>
-@php
- $test = 1;   
-@endphp
-
-{{$test}}
-
-@foreach($listings as $listing)
-  <h2>{{ $listing['title'] }}</h2>
-  <p>{{ $listing['description'] }}</p>
-@endforeach
+@if(count($listings) == 0)
+  <p>No listings found</p>
+@else
+  @foreach($listings as $listing)
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+  @endforeach
+@endif
